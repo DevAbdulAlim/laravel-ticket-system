b804e5cd6a70d2987547238f7a887839d43a23f3
Abdul Alim <aa.abdulalim13@gmail.com>
2024-09-25 21:28:50 +0600

    feat: :sparkles: add ticket management models and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,28 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isCustomer()
+    {
+        return $this->role === 'customer';
+    }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(Response::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
